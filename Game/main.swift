c237a3b4e4abe55ed3ff45f6ee1a9724a59ad8e4
bilal-dallali <82528588+bilal-dallali@users.noreply.github.com>
2023-10-10 11:49:05 +0200

    game is faster because you don't have to enter the name each time

--- Game/main.swift
+++ Game/main.swift
@@ -229,26 +229,33 @@ class Game {
     
     // Choose a character from your team
     func chooseCharacterFromYourTeam(player: Player) -> Character? {
-        print("To command, choose a character from your team by its name:")
-        player.printTeam()
-        if let choice = readLine(), let chosenCharacter = player.team.first(where: {$0.name == choice}) {
-            return chosenCharacter
+        print("To command, choose a character from your team by its number:")
+        for (index, character) in player.team.enumerated() {
+            print("\(index + 1). \(character.name) (\(character.lifePoints) life points)")
+        }
+        
+        if let choice = readLine(), let choiceInt = Int(choice), choiceInt > 0 && choiceInt <= player.team.count {
+            return player.team[choiceInt - 1]
         }
         print("Invalid choice!")
         return nil
     }
+
     
     // Choose a character from opponent's team
     func chooseCharacterFromOpponentTeam(player: Player) -> Character? {
-        print("To attack, choose a character from the opponent's team by its name:")
-        player.printTeam()
-        if let choice = readLine(), let chosenCharacter = player.team.first(where: {$0.name == choice}) {
-            return chosenCharacter
+        print("To attack, choose a character from the opponent's team by its number:")
+        for (index, character) in player.team.enumerated() {
+            print("\(index + 1). \(character.name) (\(character.lifePoints) life points)")
+        }
+        
+        if let choice = readLine(), let choiceInt = Int(choice), choiceInt > 0 && choiceInt <= player.team.count {
+            return player.team[choiceInt - 1]
         }
         print("Invalid choice!")
         return nil
     }
-    
+
     // Start the game
     func fight() {
         print("The game starts")
@@ -259,6 +266,8 @@ class Game {
         var opposingPlayer = player2
         
         while player1.teamIsAlive() && player2.teamIsAlive() {
+            roundsPlayed += 1
+            print("Round \(roundsPlayed):")
             print("\(currentPlayer.name)'s turn!")
             
             if let activeCharacter = chooseCharacterFromYourTeam(player: currentPlayer) {
@@ -275,18 +284,25 @@ class Game {
 //            print("\(player2.name) wins")
 //        }
         print(player1.teamIsAlive() ? player1.name : player2.name)
+        displayGameStatistics()
     }
     
     func displayGameStatistics() {
         print("Game statistics")
         print("Total rounds Played: \(roundsPlayed)")
-        print("\(player1.name)'s Team:")
         
+        print("\(player1.name)'s Team:")
+        if player1.team.isEmpty {
+            print("There are no characters remaining in this team")
+        }
         for character in player1.team {
             print("\(character.name) who is a \(typeString(type: typeOfCharacter(character: character))) with \(character.lifePoints) life points.")
         }
         
         print("\(player2.name)'s Team:")
+        if player2.team.isEmpty {
+            print("There are no characters remaining in this team")
+        }
         
         for character in player2.team {
             print("\(character.name) who is a \(typeString(type: typeOfCharacter(character: character))) with \(character.lifePoints) life points.")
